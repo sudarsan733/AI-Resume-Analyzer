b7583c8f4ac9c76018ef7a834b54f7da24b921e9
Sudarsan.C <csudarsan733@gmail.com>
2026-08-22 20:58:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from src.pdf_parser import extract_text_from_pdf
+
 
 st.set_page_config(
     page_title="AI Resume Analyzer",
@@ -12,14 +14,31 @@ st.write(
     "Upload your resume and compare it with a job description."
 )
 
+# Resume Upload
 resume = st.file_uploader(
     "📄 Upload your Resume",
     type=["pdf"]
 )
 
+# Job Description
 job_description = st.text_area(
     "💼 Enter the Job Description"
 )
 
-if resume and job_description:
-    st.success("Resume and Job Description received successfully!")
+# Process Resume
+if resume is not None:
+
+    resume_text = extract_text_from_pdf(resume)
+
+    st.success("Resume uploaded and text extracted successfully!")
+
+    with st.expander("📄 View Extracted Resume Text"):
+        st.text(resume_text)
+
+
+# Check both inputs
+if resume is not None and job_description:
+
+    st.success(
+        "Resume and Job Description received successfully!"
+    )
